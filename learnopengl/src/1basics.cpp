19df372b6269ec64c6b93c5aeaad459afd5b717f
Banjo <190065306+BanjoGoliath@users.noreply.github.com>
2025-02-04 16:58:01 +1030

    a commit message

--- learnopengl/src/1basics.cpp
+++ learnopengl/src/1basics.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
